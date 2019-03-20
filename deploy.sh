a28dd537f974e5693248d7751aa5683484e467c6
DindaKartika <dkartika0887@gmail.com>
2019-03-20 19:35:17 +0700

    Update

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,9 @@ ssh-add -k ~/.ssh/id_rsa &&
 cd /home/ubuntu/ecommerce_project/ProjectPortofolio
 git pull
 
-source /root/.profile
-sudo echo "DOCKERHUB_PASS" | sudo docker login --username $DOCKERHUB_USER --password-stdin
-sudo docker stop bukuku
-sudo docker rm bukuku
-sudo docker rmi dindakartika/bukuku
-sudo docker run -d --name bukuku -p 5000:5000 dindakartika/bukuku:latest
+source ~/.profile
+echo "DOCKERHUB_PASS" | docker login --username $DOCKERHUB_USER --password-stdin
+docker stop bukuku
+docker rm bukuku
+docker rmi dindakartika/bukuku
+docker run -d --name bukuku -p 5000:5000 dindakartika/bukuku:latest
